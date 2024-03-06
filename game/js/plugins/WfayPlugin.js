b9fe514b0854bcebb7b4d62148ef3d45a587f2fe
wfay42 <wfay42@gmail.com>
2024-03-05 21:49:51 -0500

    Create function addToLookupTable. Succssfully used for maid

--- game/js/plugins/WfayPlugin.js
+++ game/js/plugins/WfayPlugin.js
@@ -42,16 +42,41 @@
     femmeFataleLookupTable.set(4, 5);
     femmeFataleLookupTable.set(5, 6);
 
-    const maidLookupTable = new Map();
-    maidLookupTable.set(4, 7);
-    maidLookupTable.set(7, 8);
-
     // map states (such as clowny) to a table mapping transformations
     const stateLookupTable = new Map();
     stateLookupTable.set(clownyState, clownyLookupTable);
     stateLookupTable.set(femmeFataleState, femmeFataleLookupTable);
-    stateLookupTable.set(maidState, maidLookupTable);
 
+    /**
+     *
+     * @param {*} stateId ID of state to add to the table (like Poison)
+     * @param {*} enemyIdArr array of enemyIDs to transition through, in order.
+     * E.g. [1,2,3] means 1 can transition to 2, and 2 can transition to 3
+     */
+    const addToLookupTable = function(stateId, enemyIdArr) {
+        let lookupTable = stateLookupTable.get(stateId);
+        if (!lookupTable) {
+            lookupTable = new Map();
+            stateLookupTable.set(stateId, lookupTable);
+        }
+        let i = 0;
+        let j = 0;
+        while (i < enemyIdArr.length - 1) {
+            enemyId1 = enemyIdArr[i];
+            enemyId2 = enemyIdArr[i + 1];
+            lookupTable.set(enemyId1, enemyId2);
+            i++;
+        }
+    }
+
+    addToLookupTable(maidState, [4, 7, 8]);
+
+    /**
+     *
+     * @param {*} stateId ID of state to lookup (like Poison)
+     * @param {*} enemyId ID of enemy to lookup
+     * @returns integer enemyID, or null if not found
+     */
     const lookupFunction = function(stateId, enemyId) {
         const lookupTable = stateLookupTable.get(stateId);
         if (lookupTable) {
